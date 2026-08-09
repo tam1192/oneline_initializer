c9cf6e218efbbf137df57be4b8cdce13b8ee4f89
tam1192 <tamkame123@outlook.com>
2026-08-09 18:57:43 +0900

    style: スタイル修正

--- oli.sh
+++ oli.sh
@@ -9,105 +9,105 @@ awk_sep_rules=""
 # 引数解析: oli <var> [-i|-e|-c] [-s sep] ... [--] [file...]
 # -------------------------------------------------------------------
 while [ $# -gt 0 ]; do
-    case "$1" in
-        -h|--help)
-            echo "Usage: oli <var> [-i|-e|-c] [-s sep] ... [--] [file...]"
-            exit 0
-            ;;
-        --)
-            shift
-            break
-            ;;
-        -*)
-            # 引数の先頭がいきなりハイフンの場合は解析終了（ファイル名指定へ）
-            break
-            ;;
-        *)
-            # 1. まず変数名を取得
-            var="$1"
-            shift
+	case "$1" in
+	-h | --help)
+		echo "Usage: oli <var> [-i|-e|-c] [-s sep] ... [--] [file...]"
+		exit 0
+		;;
+	--)
+		shift
+		break
+		;;
+	-*)
+		# 引数の先頭がいきなりハイフンの場合は解析終了（ファイル名指定へ）
+		break
+		;;
+	*)
+		# 1. まず変数名を取得
+		var="$1"
+		shift
 
-            # デフォルト値のリセット
-            is_export=0
-            is_inherit=0
-            is_combine=0
-            sep_val=" "
-            [ "$var" = "PATH" ] && sep_val=":" # PATHのデフォルト
+		# デフォルト値のリセット
+		is_export=0
+		is_inherit=0
+		is_combine=0
+		sep_val=" "
+		[ "$var" = "PATH" ] && sep_val=":" # PATHのデフォルト
 
-            # 2. この変数に続くハイフン付きオプションをすべて消費するループ
-            while [ $# -gt 0 ]; do
-                case "$1" in
-                    --)
-                        shift
-                        break 2 # -- が来たら完全終了
-                        ;;
-                    -*)
-                        opts="${1#-}"
-                        shift
-                        
-                        # 1文字ずつ取り出して処理 (例: -cs -> 'c', 's')
-                        while [ -n "$opts" ]; do
-                            opt="${opts%"${opts#?}"}"
-                            opts="${opts#?}"
-                            
-                            case "$opt" in
-                                i) is_inherit=1 ;;
-                                e) is_export=1 ;;
-                                c) is_combine=1 ;;
-                                s)
-                                    if [ -n "$opts" ]; then
-                                        sep_val="$opts"
-                                        opts=""
-                                    else
-                                        sep_val="$1"
-                                        shift
-                                    fi
-                                    ;;
-                                *)
-                                    echo "エラー: 不明なオプション -$opt" >&2
-                                    exit 1
-                                    ;;
-                            esac
-                        done
-                        ;;
-                    *)
-                        # 次の変数名または引数が来たのでオプション消費ループを抜ける
-                        break
-                        ;;
-                esac
-            done
+		# 2. この変数に続くハイフン付きオプションをすべて消費するループ
+		while [ $# -gt 0 ]; do
+			case "$1" in
+			--)
+				shift
+				break 2 # -- が来たら完全終了
+				;;
+			-*)
+				opts="${1#-}"
+				shift
 
-            # 3. 組み合わせ結果から prefix / suffix を確定
-            prefix_val="${var}='"
-            suffix_val="'"
+				# 1文字ずつ取り出して処理 (例: -cs -> 'c', 's')
+				while [ -n "$opts" ]; do
+					opt="${opts%"${opts#?}"}"
+					opts="${opts#?}"
 
-            # PATH はデフォルトで export 付与
-            [ "$var" = "PATH" ] && is_export=1
+					case "$opt" in
+					i) is_inherit=1 ;;
+					e) is_export=1 ;;
+					c) is_combine=1 ;;
+					s)
+						if [ -n "$opts" ]; then
+							sep_val="$opts"
+							opts=""
+						else
+							sep_val="$1"
+							shift
+						fi
+						;;
+					*)
+						echo "エラー: 不明なオプション -$opt" >&2
+						exit 1
+						;;
+					esac
+				done
+				;;
+			*)
+				# 次の変数名または引数が来たのでオプション消費ループを抜ける
+				break
+				;;
+			esac
+		done
 
-            # -i (継承) と -c (合成) の判定
-            if [ $is_inherit -eq 1 ]; then
-                prefix_val="${var}='\$${var}${sep_val}"
-            elif [ $is_combine -eq 1 ]; then
-                suffix_val="${sep_val}\$${var}'"
-            fi
+		# 3. 組み合わせ結果から prefix / suffix を確定
+		prefix_val="${var}='"
+		suffix_val="'"
 
-            # export の付与
-            if [ $is_export -eq 1 ]; then
-                prefix_val="export ${prefix_val}"
-            fi
+		# PATH はデフォルトで export 付与
+		[ "$var" = "PATH" ] && is_export=1
 
-            # 4. その場で AWK 用コードに直書き追加
-            awk_begin="${awk_begin}    prefix[\"${var}\"] = \"${prefix_val}\";
+		# -i (継承) と -c (合成) の判定
+		if [ $is_inherit -eq 1 ]; then
+			prefix_val="${var}='\$${var}${sep_val}"
+		elif [ $is_combine -eq 1 ]; then
+			suffix_val="${sep_val}\$${var}'"
+		fi
+
+		# export の付与
+		if [ $is_export -eq 1 ]; then
+			prefix_val="export ${prefix_val}"
+		fi
+
+		# 4. その場で AWK 用コードに直書き追加
+		awk_begin="${awk_begin}    prefix[\"${var}\"] = \"${prefix_val}\";
     suffix[\"${var}\"] = \"${suffix_val}\";
 "
-            
-            # 区切り文字がスペース以外の場合のみオーバーライド規則を追加（改行を直接入れる）
-            if [ "$sep_val" != " " ]; then
-                awk_sep_rules="${awk_sep_rules}\$1 == \"${var}\" { sep = \"${sep_val}\" }
+
+		# 区切り文字がスペース以外の場合のみオーバーライド規則を追加（改行を直接入れる）
+		if [ "$sep_val" != " " ]; then
+			awk_sep_rules="${awk_sep_rules}\$1 == \"${var}\" { sep = \"${sep_val}\" }
 "
-            fi
-            ;;
-    esac
+		fi
+		;;
+	esac
 done
 
 awk_begin="${awk_begin}}
